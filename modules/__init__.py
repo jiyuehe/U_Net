de5563f34b2f65167a02b4b9aba407a62ee79f90
jiyuehe <jiyue_he@163.com>
2026-04-20 17:59:23 -0700

    .

--- modules/__init__.py
+++ modules/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import unet_minkowski
-from . import train_predict
 from . import load_data
 from . import result_analysis
+from . import train_predict
+from . import unet_minkowski
